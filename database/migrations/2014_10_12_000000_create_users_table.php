120f87978481beede7e950f6f9ed82b04aa1f270
thunder87 <Super87>
2016-02-20 22:43:11 +0100

    Initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->string('image')->default('/img/avatar.png');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('htmlheader_title')
+	Dashboard
+@endsection
+
+@section('contentheader_title')
+    Dashboard
+@endsection
+
+@section('main-content')
+
+	<div class="box">
+		<div class="box-header with-border">
+			<h3 class="box-title">Welcome</h3>
+		</div>
+		<div class="box-body">
+			You are logged in!
+		</div>
+		<!-- /.box-body -->
+	</div>
+
+@endsection
