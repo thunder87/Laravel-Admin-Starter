e70340fc336c5f3fbf7db8c155afc022b2bfc283
Thomas <djthunderburn@hotmail.com>
2016-06-15 12:41:01 +0200

    added admin user overview

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.index', compact('users'));
+    }
+}
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('htmlheader_title')
+    Admin Panel
+@endsection
+
+@section('contentheader_title')
+    Admin Panel
+@endsection
+
+@section('main-content')
+    <div class="row">
+        <div class="col-md-9">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">All Users</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <table class="table table-bordered">
+                        <tbody>
+                        <tr>
+                            <th style="width: 10px">#</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Email address</th>
+                            <th>Role</th>
+                            <th style="width: 40px"></th>
+                        </tr>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->firstname }}</td>
+                                <td>{{ $user->lastname }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td><span class="label label-primary">User</span></td>
+                                <td>
+                                    <button class="btn btn-block btn-xs btn-danger">Edit</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer clearfix">
+                    <ul class="pagination pagination-sm no-margin pull-right">
+                        <li><a href="#">«</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">»</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.box -->
+        </div>
+    </div>
+@endsection
+@section('scripts-extra')
+<script>
+    $('.btn').on('click', function() {
+        swal(
+                'Good job!',
+                'You clicked the button!',
+                'success'
+        )
+    });
+</script>
+@endsection
