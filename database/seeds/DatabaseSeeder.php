f4961337c9d223369f9f22a75145e9d3f82427f7
Thomas <djthunderburn@hotmail.com>
2016-06-15 12:41:56 +0200

    added database seed user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserTableSeeder::class);
+
+        DB::table('users')->insert([
+            'firstname' => 'Laravel',
+            'lastname' => 'Admin',
+            'email' => 'test@test.com',
+            'password' => bcrypt('test123'),
+            'image' => '/img/avatar.png',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+            'remember_token' => str_random(10),
+        ]);
     }
 }
